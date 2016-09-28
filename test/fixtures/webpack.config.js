47ba4d315fe6814052567cd8e17b44126bc324f9
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-09-28 14:54:24 -0700

    perf(tests): Common test functions in separate file
    DRY

--- test/fixtures/webpack.config.js
+++ test/fixtures/webpack.config.js
@@ -1,11 +1,8 @@
-import path from 'path';
-
 export default {
 	entry: {
 		index: './test/fixtures/index.js',
 	},
 	output: {
-		path:     path.resolve(__dirname, '../.tmp'),
 		filename: '[name].js',
 	},
 };
---
+++ test/helpers.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import merge from 'webpack-merge';
+import path from 'path';
+import promisify from 'es6-promisify';
+import webpack from 'webpack';
+
+import webpackConfig from './fixtures/webpack.config';
+
+function deleteFolder(folder) {
+	if (!fs.existsSync(folder)) {
+		return Promise.resolve();
+	}
+
+	return promisify(fs.readdir, fs)(folder)
+		.then((files) => Promise.all(files.map((file) => {
+			const curPath = [folder, file].join('/');
+
+			return promisify(fs.lstat, fs)(curPath)
+				.then((stats) => stats.isDirectory() ? deleteFolder(curPath) : promisify(fs.unlink, fs)(curPath));
+		})))
+		.then(() => promisify(fs.rmdir, fs)(folder));
+}
+
+export function runWebpack(config) {
+	const outputPath = path.resolve(__dirname, '.tmp-' + Math.random());
+
+	return deleteFolder(outputPath)
+		.then(() => promisify(webpack)(merge(webpackConfig, { output: { path: outputPath } }, config)))
+		.then((stats) => {
+			if (stats.compilation.errors.length) {
+				throw stats.compilation.errors[0];
+			}
+			return deleteFolder(outputPath)
+				.then(() => stats);
+		});
+}
